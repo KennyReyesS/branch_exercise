6a8904202ef1773d431ed5fe6a581455bdaed845
flordeciruela <wendy.reyessan@gmail.com>
2022-09-21 20:35:43 -0500

    [update] Corrections

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,18 @@ const swiper = new Swiper('.swiper', {
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination'
   },
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    prevEl: '.swiper-button-prev'
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 1
+    }
+  }
 });
